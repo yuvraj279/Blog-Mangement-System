fe91634ce23f5185924a303b4355040a5e910541
yuvraj279 <yuvrajsinghshekhawat616@gmail.com>
2023-07-04 22:06:18 +0530

    Added Conatact Us Form with the data getting stored in our database

--- BlogApp/home/views.py
+++ BlogApp/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse  
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -7,7 +9,20 @@ def home(request):
     # return HttpResponse('This is Home')
 
 def contact(request):
-    return render(request,'home/contact.html')
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content=request.POST['content']
+        print(name,email,phone,content)
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, 'Please Fill the form correctly')
+        else:
+            contact= Contact(name=name,email=email,phone=phone,content=content)
+            contact.save()
+            messages.success(request,'Your message has been Sent')
+    return render(request,'home/contact.html') 
 
 def about(request):
+    messages.success(request,'This is about')
     return render(request,'home/about.html')
